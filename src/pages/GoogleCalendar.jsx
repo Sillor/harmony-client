d0185d1520894331ba8d46198e827991b9125a1a
joshluca98 <joshluca98@gmail.com>
2024-03-28 16:13:18 -0700

    dynamically generated calendar list

--- src/pages/GoogleCalendar.jsx
+++ src/pages/GoogleCalendar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -12,75 +11,74 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-const CalendarTemplate = ({groupName}) => {
-  let [calendarId, setCalendarId] = useState('');
-
-  useEffect(() => {
-    const fetchCalendarId = async () => {
-      try {
-        if(groupName){
-          // const response = await fetch(`http://localhost:3000/calendarId/${groupName}`); // Replace with proper endpoint once available
-        } 
-        // const data = await response.json();
-        // setCalendarId(data.calendarId.substring(0, data.calendarId.length - 26));
-        if(groupName === 'group2'){
-          await setCalendarId('77d6dfc0640d4f8f0d11fb76f019496305e4e4cadd70e5fdcdea6bd8af7f47eb')
-        }
-        else if(groupName === 'group1'){
-          await setCalendarId('1608ac325d341844789f0db022f33f3c13906895cbfa7e0f981d48959ae25654')
-        }
-        else {
-          await setCalendarId('')
-        }
-      } catch (error) {
-        console.error('Error setting calendar ID:', error);
-      }
-    };
-    fetchCalendarId();
-    console.log(calendarId);
-  }, [groupName]);
-
-  const src = `https://calendar.google.com/calendar/embed?src=${calendarId}%40group.calendar.google.com&ctz=America%2FLos_Angeles`;
-
-  return ( 
-    <>
-        <iframe
-            className='border-double border-8 border-black dark:border-gray-700 rounded-lg'
-            src={src}
-            style={{ width: '75vw', height: '90vh', borderRadius: 15}}
-            title='calendar'
-        ></iframe>
-    </>
-  )
-}
-
-const GoogleCalendar = ({username}) => {
+const GoogleCalendar = () => {
 
   const [groupName, setGroupName] = React.useState("")
+  console.log(groupName);
+
+  const [srcId, setSrcId] = React.useState('harmonyapp2024%40gmail.com')
+
+
+  let [calendars, setCalendars] = useState([
+    {
+      "name": "harmonyapp2024@gmail.com",
+      "id": "harmonyapp2024@gmail.com"
+    }
+  ]);
+
+  useEffect(() => {
+    const fetchCalendars = async () => {
+      try {
+        console.log('fetch is running');
+        const response = await fetch(`http://localhost:5000/api/calendar/listcalendars`);
+        console.log('fetch response:', response);
+        const data = await response.json();
+        console.log('fetch data:', data);
+        setCalendars(data.filter(calendar => calendar.name !== "harmonyapp2024@gmail.com"));
+      } catch (error) {
+        console.error('Error fetching calendars:', error);
+      }
+    };
+    fetchCalendars();
+  }, []);
+  const sortedCalendars = [...calendars].sort((a, b) => a.name.localeCompare(b.name));
+
+
+  useEffect(() => {
+    const selectedCalendar = calendars.find(calendar => calendar.name === groupName);
+    if (selectedCalendar) {
+      setSrcId(selectedCalendar.id.split('@')[0]);
+    }
+  }, [groupName, calendars]);
   
-  // Some code to fetch list of users groups from the sql database and add them to the following array will be added at some point
-  const [calendarList, setCalendarList] = React.useState([
-    'group1',
-    'group2',
-    'group3'
-  ])
+  
+
+  const src = calendars.length > 0 ? `https://calendar.google.com/calendar/embed?src=${srcId}%40group.calendar.google.com&ctz=America%2FLos_Angeles` : '';
+
 
 
   return (
-    <div className=''>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <div style={{ display: 'flex', justifyContent: 'center' }}><Button className='w-full bg-black text-white dark:bg-gray-700 mb-1' variant="outline">{username} Select Calendar</Button></div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-30">
-          <DropdownMenuRadioGroup value={groupName} onValueChange={setGroupName}>
-            {calendarList.map(calendar => (
-              <DropdownMenuRadioItem key={calendar} value={calendar}>{calendar}</DropdownMenuRadioItem>
-            ))}
-          </DropdownMenuRadioGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <CalendarTemplate groupName={groupName} />
+    <div className='flex justify-center flex-col'>
+      
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button style={{ width: '80vw'}} className='bg-black text-white dark:bg-gray-700 mb-2'>Select Calendar</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-30">
+            <DropdownMenuRadioGroup value={groupName} onValueChange={setGroupName}>
+              {sortedCalendars.map(calendar => (
+                <DropdownMenuRadioItem key={calendar.id} value={calendar.name}>{calendar.name}</DropdownMenuRadioItem>
+              ))}
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      
+      <iframe
+        className='border-double border-8 border-black dark:border-gray-700 rounded-lg'
+        src={src}
+        style={{ width: '80vw', height: '90vh', borderRadius: 15 }}
+        title='calendar'
+      ></iframe>
       <pre className='text-center mt-3'>To edit or delete calendar events, press the button on the bottom right of the calendar labeled "Google Calendar" or use the dashboard. -----------------------------------------^</pre>
     </div>
   )
